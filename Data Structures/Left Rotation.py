148c22037e46137c7f9d62421eb778cd00a39cc4
wescley1 <wescleydecarvalho@hotmail.com>
2024-04-06 12:23:59 -0300

    attempting left rotation

---
+++ Data Structures/Left Rotation.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 11:47:28 2024
+
+@author: wescl
+"""
+
+vec = [1, 2, 3, 4, 5]
+
+def rotateLeft(d, arr):
+    aux = arr    
+    size = len(arr)
+    ans = [0]*size
+    for execution in range(d):           
+        for x in range(size):
+            i = (x+1)%size
+            ans[x] = aux[i] 
+            
+            
+        aux = ans            
+    return ans
+    
+    
+print (rotateLeft(4,vec))
+#for x in range(len(vec)):
+#    print(str((x-1)%5)+": "+str(vec[x]))
+
+#for x in range(5):
+#    print(x)
